9ce6c9c86c441c3821c2fa04ffb98409ae34f677
Zhengyu Wang <zhengyu.wang@cygni.se>
2020-04-14 10:47:26 +0200

    Add pytest.fixture for a list of sample data

--- tests/test_fixtures.py
+++ tests/test_fixtures.py
@@ -8,5 +8,17 @@ def sample_data():
     return 1
 
 
+@pytest.fixture()
+def a_group_sample_data():
+    return [1,5,15,42,9,94,1776]
+
+
+@pytest.fixture()
 def test_sample_data(sample_data):
     assert int_to_roman(sample_data) == "I"
+
+
+def test_a_group_sample_data(a_group_sample_data):
+    expected = [ "I",  "V",  "XV", "XLII", "IX", "XCIV", "MDCCLXXVI"]
+    for i in range(len(a_group_sample_data)):
+        assert int_to_roman(a_group_sample_data[i]) == expected[i]
